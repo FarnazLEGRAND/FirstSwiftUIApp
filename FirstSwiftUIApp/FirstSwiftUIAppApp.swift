500f960be41602ae78b4e3b2a9c40172901b2838
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-08-31 17:47:31 +0200

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Farnaz Rismanchian on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
